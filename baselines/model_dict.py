99979a5764a327c038468e802ed22994c65a54e5
Gringham <christoph_leiter@web.de>
2023-08-21 17:11:57 +0200

    base_name property no longer required for TheBloke models

--- baselines/model_dict.py
+++ baselines/model_dict.py
@@ -33,10 +33,9 @@ def load_peft_model(model_name, orig):
     return model, tokenizer
 
 
-def load_gptq_model(model_name, base_name, trust_remote_code=False, quantize_config=None,inject_fused_attention=True):
+def load_gptq_model(model_name, trust_remote_code=False, quantize_config=None,inject_fused_attention=True):
     model = AutoGPTQForCausalLM.from_quantized(
         model_name,
-        model_basename=base_name,
         use_safetensors=True,
         trust_remote_code=trust_remote_code,
         device="cuda:0",
@@ -64,14 +63,14 @@ def load_from_catalogue(model_name):
         },
         "TheBloke/guanaco-65B-GPTQ": {
             "load_method": lambda x: load_gptq_model(
-                x, base_name="Guanaco-65B-GPTQ-4bit.act-order", trust_remote_code=True, #might not need the trust remote code
+                x, trust_remote_code=True, #might not need the trust remote code
             ),
             "user_prompt": "### Human:",
             "assistant_prompt": "### Assistant:",
         },
         "TheBloke/WizardLM-13B-V1.1-GPTQ": {
             "load_method": lambda x: load_gptq_model(
-                x, base_name="wizardlm-13b-v1.1-GPTQ-4bit-128g.no-act.order", trust_remote_code=True, #might not need the trust remote code
+                x, trust_remote_code=True, #might not need the trust remote code
             ),
             "user_prompt": "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.\n\n USER: ",
             "assistant_prompt": "Assistant: ",
